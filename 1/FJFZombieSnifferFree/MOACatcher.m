fa379b104664b902dda8425774f1d2e52fb1c166
邓江洲 <dengjiangzhouReal@gmail.com>
2022-07-16 23:15:41 +0800

    code Opt

--- 1/FJFZombieSnifferFree/MOACatcher.m
+++ 1/FJFZombieSnifferFree/MOACatcher.m
@@ -14,16 +14,19 @@
 @implementation MOACatcher
 - (BOOL)respondsToSelector: (SEL)aSelector
 {
+    NSLog(@"0_1");
     return [self.originClass instancesRespondToSelector:aSelector];
 }
 
 - (NSMethodSignature *)methodSignatureForSelector: (SEL)sel
 {
+    NSLog(@"0_2");
     return [self.originClass instanceMethodSignatureForSelector:sel];
 }
 
 - (void)forwardInvocation: (NSInvocation *)invocation
 {
+    NSLog(@"0_3");
     [self _throwMessageSentExceptionWithSelector: invocation.selector];
 }
 
@@ -31,48 +34,56 @@
 #define MOAZombieThrowMesssageSentException() [self _throwMessageSentExceptionWithSelector: _cmd]
 - (Class)class
 {
+    NSLog(@"0_4");
     MOAZombieThrowMesssageSentException();
     return nil;
 }
 
 - (BOOL)isEqual:(id)object
 {
+    NSLog(@"0_5");
     MOAZombieThrowMesssageSentException();
     return NO;
 }
 
 - (NSUInteger)hash
 {
+    NSLog(@"0_6");
     MOAZombieThrowMesssageSentException();
     return 0;
 }
 
 - (id)self
 {
+    NSLog(@"0_7");
     MOAZombieThrowMesssageSentException();
     return nil;
 }
 
 - (BOOL)isKindOfClass:(Class)aClass
 {
+    NSLog(@"0_8");
     MOAZombieThrowMesssageSentException();
     return NO;
 }
 
 - (BOOL)isMemberOfClass:(Class)aClass
 {
+    NSLog(@"0_9");
     MOAZombieThrowMesssageSentException();
     return NO;
 }
 
 - (BOOL)conformsToProtocol:(Protocol *)aProtocol
 {
+    NSLog(@"0_10");
     MOAZombieThrowMesssageSentException();
     return NO;
 }
 
 - (BOOL)isProxy
 {
+    NSLog(@"0_11");
     MOAZombieThrowMesssageSentException();
     
     return NO;
@@ -80,41 +91,48 @@
 
 - (id)retain
 {
+    NSLog(@"0_12");
     MOAZombieThrowMesssageSentException();
     return nil;
 }
 
 - (oneway void)release
 {
+    NSLog(@"0_13");
     MOAZombieThrowMesssageSentException();
 }
 
 - (id)autorelease
 {
+    NSLog(@"0_14");
     MOAZombieThrowMesssageSentException();
     return nil;
 }
 
 - (void)dealloc
 {
+    NSLog(@"0_15");
     MOAZombieThrowMesssageSentException();
     [super dealloc];
 }
 
 - (NSUInteger)retainCount
 {
+    NSLog(@"0_16");
     MOAZombieThrowMesssageSentException();
     return 0;
 }
 
 - (NSZone *)zone
 {
+    NSLog(@"0_17");
     MOAZombieThrowMesssageSentException();
     return nil;
 }
 
 - (NSString *)description
 {
+    NSLog(@"0_18");
     MOAZombieThrowMesssageSentException();
     return nil;
 }
